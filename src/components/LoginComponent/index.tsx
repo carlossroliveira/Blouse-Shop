05c897e7d9035877ee93e1e29f41bab831b1a8d5
carlos oliveira <carlos.sroliveira@hotmail.com>
2022-07-27 17:21:17 -0300

    Changes

--- src/components/LoginComponent/index.tsx
+++ src/components/LoginComponent/index.tsx
@@ -1,29 +1,57 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../../hooks/useAuth';
+import React, { useState } from 'react';
+/* import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth'; */
 
 export const LoginComponent = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  /* 
   const NAVIGATION = useNavigate();
-  const auth = useAuth();
+  const auth = useAuth(); */
 
-  async function onFinish(values: { email: string; password: string }) {
+  const handleSubmita = (event: any) => {
+    event.preventDefault();
+
+    console.log(email);
+    console.log(password);
+    /* try {
+      await auth.authenticate(email, password);
+
+      NAVIGATION('/profile');
+    } catch (err) {
+      alert(err);
+    } */
+  };
+
+  /*  async function onFinish(event: any, email: string, password: string) {
+    event.preventDefault();
     try {
-      await auth.authenticate(values.email, values.password);
+      await auth.authenticate(email, password);
 
       NAVIGATION('/profile');
     } catch (err) {
       alert(err);
     }
-  }
+  } */
 
   return (
     <>
-      <form onSubmit={onFinish}>
+      <form onSubmit={handleSubmita}>
         <div>
-          <input type="text" placeholder="email" />
+          <input
+            type="text"
+            placeholder="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </div>
         <div>
-          <input type="text" placeholder="Senha" />
+          <input
+            type="text"
+            placeholder="Senha"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
         </div>
         <button type="submit">Logar</button>
       </form>
