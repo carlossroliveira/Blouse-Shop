5707f469de854da8f019bf75e7cc9143059c0127
carlos oliveira <carlos.sroliveira@hotmail.com>
2022-08-08 15:59:52 -0300

    unit test setup

--- src/__tests__/Layout/main.test.tsx
+++ src/__tests__/Layout/main.test.tsx
@@ -3,22 +3,19 @@
 // -------------------------------------------------
 import React from 'react';
 
-import 'jest-styled-components';
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 // -------------------------------------------------
 // Components
 // -------------------------------------------------
 import { Protected } from '../../components/Protected';
-// -------------------------------------------------
-// Utils
-// -------------------------------------------------
-import WithThemeComponent from '../../utils/withThemeComponent';
-
-const LayoutWithTheme = WithThemeComponent(Protected);
 
 describe('Layout Testing', () => {
   it('Should...', () => {
-    render(<LayoutWithTheme />);
+    render(
+      <Protected>
+        <p>aaa</p>
+      </Protected>,
+    );
   });
 });
