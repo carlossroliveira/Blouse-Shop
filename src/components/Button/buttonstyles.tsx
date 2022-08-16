db77336ffc35e462bd50cad5bd3a23982dc71070
carlos oliveira <carlos.sroliveira@hotmail.com>
2022-08-16 18:12:22 -0300

    implementation of a button component

---
+++ src/components/Button/buttonstyles.tsx
@@ -0,0 +1,8 @@
+// -------------------------------------------------
+// Packages
+// -------------------------------------------------
+import styled from 'styled-components';
+
+export const ContainerSC = styled.section`
+  background-color: tomato;
+`;
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+// -------------------------------------------------
+// Packages
+// -------------------------------------------------
+import React from 'react';
+// -------------------------------------------------
+// Styles
+// -------------------------------------------------
+import { ContainerSC } from './buttonstyles';
+
+export const Button = () => {
+  return <ContainerSC>index</ContainerSC>;
+};
