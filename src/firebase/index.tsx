f3699660d0a00d188dadaefad732aa91be5284c7
carlos oliveira <carlos.sroliveira@hotmail.com>
2022-08-24 18:32:25 -0300

    Implementação do firebase

---
+++ src/firebase/index.tsx
@@ -0,0 +1,107 @@
+// -------------------------------------------------
+// Packages
+// -------------------------------------------------
+import React, { useState } from 'react';
+// -------------------------------------------------
+// Firebase
+// -------------------------------------------------
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  GithubAuthProvider,
+} from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD3Bom7nwuCnw2JPQtYE96AOLR1njQHOCw',
+  authDomain: 'testing-ebcd0.firebaseapp.com',
+  projectId: 'testing-ebcd0',
+  storageBucket: 'testing-ebcd0.appspot.com',
+  messagingSenderId: '39028439000',
+  appId: '1:39028439000:web:4f97b5acbb82f1f0a6a4ad',
+};
+
+const app = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+const providera = new GithubAuthProvider();
+
+const auth = getAuth(app);
+
+export const Firebase = (): JSX.Element => {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const gooogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        const user = result.user;
+        console.log(user, 'Popup');
+        console.log(token, 'Popup');
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        console.log(errorCode);
+      });
+  };
+
+  const EmailESenha = (e: any) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, senha)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user, 'Senha');
+        setEmail('');
+        setSenha('');
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        console.log(errorCode);
+      });
+  };
+
+  const githubb = () => {
+    signInWithPopup(auth, providera)
+      .then((result) => {
+        const credential = GithubAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        const user = result.user;
+        console.log(user);
+        console.log(token);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
+
+  return (
+    <>
+      <pre>{JSON.stringify(email)}</pre>
+
+      <form onSubmit={EmailESenha}>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+        />
+        <button>Form</button>
+      </form>
+
+      <button onClick={gooogle}>Google</button>
+      <button onClick={githubb}>GitHub</button>
+    </>
+  );
+};
