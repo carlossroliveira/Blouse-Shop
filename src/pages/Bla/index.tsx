cac4a075966df7188688204b75c97b9488f3959c
carlos oliveira <carlos.sroliveira@hotmail.com>
2022-08-30 08:55:44 -0300

    Testes de rotas

--- src/pages/Bla/index.tsx
+++ src/pages/Bla/index.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
+import axios from 'axios';
+/* import { Mirage } from '../../mirage'; */
+
 export const Bla = () => {
+  const [users, setUsers] = useState([]);
   const { id } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`/list/${id}`)
+      .then((response) => response.data)
+      .then((data) => setUsers(data));
+  }, [id]);
+
+  /* Mirage(); */
+
+  console.log(users.map((item: any) => item.name));
+
   return (
     <>
-      <h1>Bla bla {id}</h1>
+      <h1>
+        {users.map((item: any) => (
+          <>
+            <p>{item.name}</p>
+          </>
+        ))}
+      </h1>
+      {id}
     </>
   );
 };
