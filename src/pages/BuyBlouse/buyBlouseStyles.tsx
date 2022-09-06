6cfad48b4d8ed94513a0b2f5f39acae38dc18cf6
carlos oliveira <carlos.sroliveira@hotmail.com>
2022-09-06 17:06:51 -0300

    Ajustes pagina das camisas

--- src/pages/BuyBlouse/buyBlouseStyles.tsx
+++ src/pages/BuyBlouse/buyBlouseStyles.tsx
@@ -6,11 +6,79 @@ import styled from 'styled-components';
 export const ContainerSC = styled.section`
   display: flex;
   flex-wrap: wrap;
+  align-items: center;
+
+  height: calc(100vh - 200px);
 `;
 
 export const DivSC = styled.div`
   gap: 1rem;
   display: flex;
   width: 60rem;
+
   margin: 0 auto;
+`;
+
+export const ImgSC = styled.img`
+  width: 30rem;
+`;
+
+export const DivTwoSC = styled.div``;
+
+export const ParagraphSC = styled.p<{
+  Inactive?: boolean;
+  Installments?: boolean;
+  InstallmentsTwo?: boolean;
+  Info?: boolean;
+  Title?: boolean;
+}>`
+  color: black;
+
+  span {
+    color: ${({ theme }) => theme.color.secondary};
+    font-weight: bold;
+  }
+
+  text-decoration: ${({ Inactive }) =>
+    Inactive &&
+    'font-weight: 100; text-decoration: line-through; color: #8c8c8c;'};
+
+  ${({ Installments }) =>
+    Installments &&
+    'color: #90caf9; font-size: 2rem; font-weight: 700; line-height: 24px; margin: 0'};
+
+  ${({ InstallmentsTwo }) =>
+    InstallmentsTwo &&
+    'color: #90caf9; font-size: 1.2rem; font-weight: 700; line-height: 24px; margin-top: 1rem'};
+
+  ${({ Info: info }) =>
+    info &&
+    'color: #04d483;font-size: 1.4rem; font-weight: 600; text-transform: uppercase;padding: 0;'};
+
+  ${({ Title: title }) =>
+    title && 'color: #383838;font-size: 1.2rem; font-weight: 700;'};
+`;
+
+export const DivFourSC = styled.div`
+  margin-top: 30px;
+`;
+
+export const DivThreeSC = styled.div`
+  margin-top: 40px;
+`;
+
+export const SizeSC = styled.span`
+  display: flex;
+  padding: 0 7px;
+  height: 28px;
+  min-width: 28px;
+  text-align: center;
+  font-size: 0.6875rem;
+  line-height: 26px;
+  border-radius: 20px;
+  font-weight: 700;
+  text-decoration: none;
+  border: 1px solid #ccc;
+  width: 1rem;
+  margin-top: 5px;
 `;
--- src/pages/BuyBlouse/index.tsx
+++ src/pages/BuyBlouse/index.tsx
@@ -1,14 +1,37 @@
+// -------------------------------------------------
+// Packages
+// -------------------------------------------------
 import React from 'react';
-import { useFetch } from '../../hooks/useFetch';
 import { useParams } from 'react-router-dom';
-
-import { IBProps } from '../../utils/types';
+import { FaShippingFast } from 'react-icons/fa';
+// -------------------------------------------------
+// Hooks
+// -------------------------------------------------
+import { useFetch } from '../../hooks/useFetch';
+// -------------------------------------------------
+// Components
+// -------------------------------------------------
 import { NamePage } from '../../components/NamePage';
-import { ContainerSC, DivSC } from './buyBlouseStyles';
+// -------------------------------------------------
+// Types
+// -------------------------------------------------
+import { IBProps } from '../../utils/types';
+// -------------------------------------------------
+// Styles
+// -------------------------------------------------
+import {
+  ContainerSC,
+  DivFourSC,
+  DivSC,
+  DivThreeSC,
+  DivTwoSC,
+  ImgSC,
+  ParagraphSC,
+  SizeSC,
+} from './buyBlouseStyles';
 
 export const BuyBlouse = () => {
   const { id } = useParams();
-
   const { data } = useFetch<IBProps>(`http://localhost:5000/posts/${id}`);
 
   return (
@@ -16,20 +39,56 @@ export const BuyBlouse = () => {
       <NamePage title="Camisas" />
 
       <DivSC>
-        <img
+        <ImgSC
           src={`${data?.foto?.map((item) => item.src)}`}
           alt="Camisa de time"
         />
 
-        <p>{data?.foto?.map((item) => item.titulo)}</p>
+        <DivTwoSC>
+          {data?.info && (
+            <ParagraphSC Info>
+              <FaShippingFast /> {data?.info}
+            </ParagraphSC>
+          )}
 
-        <p>{data?.marca}</p>
-        {/*  <p>{data?.preco}</p> */}
+          <ParagraphSC Title>
+            {data?.foto?.map((item) => item.titulo)}
+          </ParagraphSC>
 
-        <p>{data?.tamanho}</p>
-        <p>{data?.tecido}</p>
+          <DivFourSC>
+            {data?.preco?.map((item, index) => (
+              <>
+                <ParagraphSC Inactive key={index}>
+                  R$ {item.valorInativo}
+                </ParagraphSC>
 
-        <p>{data?.descricao}</p>
+                <ParagraphSC Installments>R$ {item.valorInicial}</ParagraphSC>
+
+                <ParagraphSC InstallmentsTwo>
+                  Ou {item.valorIniParcelado}x de R$ {item.valorInaParcelado}{' '}
+                  sem juros
+                </ParagraphSC>
+              </>
+            ))}
+          </DivFourSC>
+
+          <DivThreeSC>
+            <ParagraphSC>
+              <span>Tamanho:</span>
+              <SizeSC>{data?.tamanho?.slice(0, 1)}</SizeSC>
+              <SizeSC>{data?.tamanho?.slice(2, 4)}</SizeSC>
+            </ParagraphSC>
+            <ParagraphSC>
+              <span>Tercido:</span> {data?.tecido}
+            </ParagraphSC>
+            <ParagraphSC>
+              <span>Marca:</span> {data?.marca}
+            </ParagraphSC>
+            <ParagraphSC>
+              <span>Descrição:</span> {data?.descricao}
+            </ParagraphSC>
+          </DivThreeSC>
+        </DivTwoSC>
       </DivSC>
     </ContainerSC>
   );
