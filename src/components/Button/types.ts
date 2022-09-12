3f51c0eaf444ea935e487e6fb7cb3264f14cc130
carlos oliveira <carlos.sroliveira@hotmail.com>
2022-09-12 17:09:26 -0300

    ajustes, remoção de linha em vazia

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -5,7 +5,6 @@ import { ButtonHTMLAttributes } from 'react';
 // -------------------------------------------------
 // Types
 // -------------------------------------------------
-
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   text: string;
 }
